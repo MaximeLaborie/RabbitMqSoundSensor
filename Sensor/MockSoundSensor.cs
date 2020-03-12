0d5c883c0723e6d3641371803968cc09c6ae6032
Maxime Laborie <mla@softfluent.com>
2020-03-12 11:05:02 +0100

    Add a mock sensor

--- Sensor/MockSoundSensor.cs
+++ Sensor/MockSoundSensor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Timers;
 
 namespace Sensor
 {
@@ -8,9 +7,21 @@ namespace Sensor
     {
         public event EventHandler<short> PeakAcquired;
 
+        private Random _random;
+
         public void Start(int sampleEveryMs)
         {
+            _random = new Random();
+            Timer aTimer = new Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTick);
+            aTimer.Interval = sampleEveryMs;
+            aTimer.Enabled = true;
+        }
 
+        private void OnTick(object sender, ElapsedEventArgs e)
+        {
+            short val = (short)_random.Next(0, short.MaxValue - 1000);
+            PeakAcquired?.Invoke(this, val);
         }
     }
 }
